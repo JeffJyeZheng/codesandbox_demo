53fac18fac6fb90b25087d9f7c962af96205fef0
JeffZheng <jye22059758@gmail.com>
2021-06-08 16:26:04 +0800

    Initial commit

---
+++ src/components/HellowWorld.vue.ts
@@ -0,0 +1,9 @@
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "PlatformTransferSetting",
+  components: {}
+})
+export default class HellowWorld extends Vue {
+  private msg: string = "aaaa11";
+}
